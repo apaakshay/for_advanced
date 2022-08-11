ca5c9d6d6cac12d8375d73913eece23308e15ab6
apaakshay <72438238+apaakshay@users.noreply.github.com>
2022-08-11 14:10:14 +0530

    first

--- calculator.py
+++ calculator.py
@@ -10,19 +10,25 @@ print("2. Substraction ")
 print("3. Multiplication")
 print("4. Division")
 print("5. Modulus")
-sum=input("Please choose an option from these(1,2,3,4,5): ")
-if sum=="1":
-    print("1.Addition")
-    print("the sum of two numbers: ",num1+num2)
-if sum=="2":
-    print("2.Substraction")
-    print("the substraction of two numbers: ", num1 - num2)
-if sum=="3":
-    print("3.Multiplication")
-    print("the Multicplication of two numbers: ", num1 * num2)
-if sum=="4":
-    print("4.Division")
-    print("the divison of two numbers: ", num1 / num2)
-if sum=="5":
-    print("5.Modulus")
-    print("the modulus of two numbers: ", num1 % num2)
+result=0
+operator=input("Please choose an option from these(1,2,3,4,5): ")
+if operator=="1":
+    replace1="Addition"
+    result=num1+num2
+
+if operator=="2":
+    replace1="Substraction"
+    result=num1-num2
+
+if operator=="3":
+    replace1="Multiplication"
+    result=num1*num2
+
+if operator=="4":
+    replace1="Division"
+    result=num1//num2
+
+if operator=="5":
+    replace1="Modulus"
+    result=num1%num2
+print("The Result Of",replace1,"of",num1,"and",num2,"is",result)
